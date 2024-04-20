4fc6bca8437d4e00acd608260ec794c104704d66
MelinaKatsoulakis <melinakatsoulakis@hotmail.co.uk>
2024-04-20 14:56:56 +0100

    Added search functionality through the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey = "f7a304o60c11ec8b84509fafa7844td6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector(".searchForm");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
